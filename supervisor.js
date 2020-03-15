a85d766de03866d59253c46ff7e0016ee0a78492
Magen Bouchard <Magen.Bouchard@gmail.com>
2020-03-15 17:35:36 -0400

    added comments to explain logic

--- supervisor.js
+++ supervisor.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 //connection info
-let connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
